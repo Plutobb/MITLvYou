d021c88cee21787919eba5b62e4f0a0f7b41db19
liuwj <308729727@qq.com>
2022-08-02 15:38:46 +0800

    登录模块

--- src/main/java/com/example/mit_lvyou/mapper/UserMapper.java
+++ src/main/java/com/example/mit_lvyou/mapper/UserMapper.java
@@ -3,8 +3,11 @@ package com.example.mit_lvyou.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.example.mit_lvyou.entity.TravelInfoBean;
 import com.example.mit_lvyou.entity.UserBean;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 /**
  * @author hongbaozhang
@@ -14,4 +17,10 @@ import org.apache.ibatis.annotations.Select;
 public interface UserMapper extends BaseMapper<UserBean> {
     @Select("select * from user_table as user join travel_table as travel on user.user_id = travel.userid where user.user_id = #{userId}")
     TravelInfoBean selectUserTravelById(Integer userId);
+
+    @Select("select * from user_table")
+    List<UserBean> selectAllUser();
+
+    @Select("select * from user_table where nickname = #{nickName}")
+    UserBean selectUserByNickName(String nickName);
 }
